0df38eca8d080cf26b2d8ddb779d7cf96a3a584f
Excelyynx <excelyynxl@gmail.com>
2026-03-23 20:35:37 +0800

    implement api calls for signup, login, logout, updateProfile, checkAuth,

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,32 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/user.model.js';
+
+// protectRoute make sure only authenticated user can update their own profile
+export const protectRoute = async (req, res, next) => {
+    try {
+        const token = req.cookies.jwt;
+
+        if (!token) {
+            return res.status(401).json({ message: "Unauthorised - No Token Provided."});
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        if (!decoded) {
+            return res.status(401).json({ mesage: "Unauthorised - Invalid Token."})
+        }
+
+        const user = await User.findById(decoded.userId).select("-password");
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found"});
+        }
+
+        req.user = user; // attach the user to the req so that updateProfile function has access to the user
+
+        next(); // call onto the next function (eg: updateProfile)
+    } catch (error) {
+        console.log("Error in protectRoute middleware");
+        res.status(500).json({ message: "Internal Server Error"});
+    }
+}
