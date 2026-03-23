38d090b6b029beac68749fec871a33cac4e48de7
Excelyynx <excelyynxl@gmail.com>
2026-03-23 20:34:06 +0800

    method to generate jwt token

---
+++ backend/src/lib/utils.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+
+// generate a token suring signing up
+export const generateToken = (userId, res) => {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+        expiresIn: "7d", // meaning a token only last for 7 days
+    });
+
+    // send token to user in a cookie
+    res.cookie("jwt", token, {
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in millisecs 
+        httpOnly: true, // prevent XSS attacks cross-site scripting attacks 
+        smaeSite: "strict", // CSRF attacks cross-site request forgery attacks
+        secure: process.env.NODE_ENV !== "development", 
+    });
+
+    return token;
+}
