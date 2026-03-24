50f6cea299ec19e123df61f088f51aaff8b1a826
Excelyynx <excelyynxl@gmail.com>
2026-03-24 23:25:12 +0800

    increase upload size to allow sender to send bigger payload (iamge + text message)

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,9 @@ dotenv.config();
 const PORT = process.env.PORT;
 const app = express();
 
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 
 // cors MUST be used BEFORE routes
