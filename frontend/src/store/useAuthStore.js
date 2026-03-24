63a0180c7a2b95e1044201e1db3d0be6976d3a18
Excelyynx <excelyynxl@gmail.com>
2026-03-24 11:44:47 +0800

    create useAuthStore for global state management

---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,23 @@
+import { create } from 'zustand';
+import { axiosInstance } from '../lib/axios';
+
+// zustand is a global state manager
+export const useAuthStore = create((set) => ({
+    authUser: null,
+    isSigningUp: false,
+    isLoggingIn: false,
+    isUpdatingProfile: false,
+    isCheckingAuth: true,
+
+    checkAuth: async () => {
+        try {
+            const res = await axiosInstance.get('/auth/check');
+            set({ authUser: res.data });
+        } catch (error) {
+            console.error('Error in checkAuth: ', error);
+            set({ authUser: null });
+        } finally {
+            set({ isCheckingAuth: false });
+        }
+    }
+}))
